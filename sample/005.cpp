373789ab3cef4e28e98c9860d93e782670209843
Yumax-panda <yuma0wall@gmail.com>
2023-06-01 03:30:13 +0900

    renamed not to duplicate builtin func

--- sample/005.cpp
+++ sample/005.cpp
@@ -5,13 +5,13 @@ using namespace std;
 bool check (int a, int A, int B);
 
 int main () {
-    int  N, A, B, sum;
+    int  N, A, B, total;
     cin >> N >> A >> B;
 
     for(int k=1; k<= N; ++k) {
-        if (check(k, A, B)) sum += k;
+        if (check(k, A, B)) total += k;
     }
-    cout << sum << endl;
+    cout << total << endl;
     return 0;
 }
 
